8afe0636a3a6f8338fd038c7c4ead6118d9a6ead
saka508 <saka508@users.noreply.github.com>
2026-09-09 21:49:16 +0900

    Fix NativeWind web export

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,29 @@
+const fs = require("fs");
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+
+const cacheDir = path.join(
+  __dirname,
+  "node_modules",
+  "react-native-css-interop",
+  ".cache"
+);
+
+const webCss = path.join(cacheDir, "web.css");
+
+try {
+  fs.mkdirSync(cacheDir, { recursive: true });
+  if (!fs.existsSync(webCss)) {
+    fs.writeFileSync(webCss, "");
+  }
+} catch (error) {
+  console.warn("[nativewind] Unable to prepare css-interop cache", error);
+}
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
   forceWriteFileSystem: true,
 });
